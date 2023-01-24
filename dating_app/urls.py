f794cf077d6715093c13f6504d30e7354f64b89d
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-01-24 08:40:42 +0500

    added partnet account page

--- dating_app/urls.py
+++ dating_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'dating_app'
 
 urlpatterns = [
-    path('', views.dating, name='dating')
+    path('', views.dating, name='dating'),
+
+    # Профиль другого пользователя
+    path('<int:user_id>/', views.partner_account, name='partner_account'),
 ]
--- dating_app/views.py
+++ dating_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from datetime import datetime
@@ -10,3 +10,9 @@ def dating(request):
 	profiles = User.objects.order_by('-last_login')
 	date_now = datetime.now()
 	return render(request, 'dating_app/dating.html', {'profiles':profiles, 'date_now':date_now})
+
+
+@login_required
+def partner_account(request, user_id):
+	partner_account = get_object_or_404(User, pk=user_id)
+	return render(request, 'dating_app/partner_account.html', {'partner_account':partner_account})

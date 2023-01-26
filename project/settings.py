4aba9ff5feb34b94b6dc012a8ba5cb5060c7f52a
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-01-26 16:09:46 +0500

    test cache

--- project/settings.py
+++ project/settings.py
@@ -38,14 +38,14 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware', # Debug
+    #'debug_toolbar.middleware.DebugToolbarMiddleware', # Debug
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+# ]
 
 ROOT_URLCONF = 'project.urls'
 

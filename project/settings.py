b5bac1fc27c6cbe88a2499fa95a1910b9bc5b7db
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-01-26 22:05:26 +0500

    hash check

--- project/settings.py
+++ project/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
 STATIC_URL = '/static/'
 
 # Media

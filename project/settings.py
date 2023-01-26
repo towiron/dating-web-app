ac5074819b9981ec5a568e99d8a82bd7d5dfc98b
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-01-26 21:31:26 +0500

    ui update

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    #'debug_toolbar.middleware.DebugToolbarMiddleware', # Debug
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware', # Debug
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',

afbb5024c65bbaf726950c5f2749d2f653adba0e
Batyr Shirmatov <batyrshirmatov@MacBook-Air-Batyr.local>
2023-01-26 16:20:15 +0500

    test cache

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Debug
-    'debug_toolbar',
+    # 'debug_toolbar',
     # Apps
     'chat_app',
     'dating_app',
